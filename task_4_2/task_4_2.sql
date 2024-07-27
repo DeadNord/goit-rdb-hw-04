1dc18507416ecaa592fed339a08245c463785f38
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2024-07-27 11:42:08 +0200

    fix tasks

--- task_4_2/task_4_2.sql
+++ task_4_2/task_4_2.sql
@@ -1,17 +1,9 @@
 SELECT COUNT(*)
-FROM 
-    order_details od
-INNER JOIN 
-    orders o ON od.order_id = o.id
-INNER JOIN 
-    customers c ON o.customer_id = c.id
-INNER JOIN 
-    products p ON od.product_id = p.id
-LEFT JOIN 
-    categories cat ON p.category_id = cat.id
-LEFT JOIN 
-    employees e ON o.employee_id = e.employee_id
-RIGHT JOIN 
-    shippers sh ON o.shipper_id = sh.id
-RIGHT JOIN 
-    suppliers s ON p.supplier_id = s.id;
+FROM  order_details od
+LEFT JOIN  orders o ON od.order_id = o.id
+RIGHT JOIN  customers c ON o.customer_id = c.id
+LEFT JOIN   products p ON od.product_id = p.id
+LEFT JOIN    categories cat ON p.category_id = cat.id
+LEFT JOIN   employees e ON o.employee_id = e.employee_id
+LEFT JOIN   shippers sh ON o.shipper_id = sh.id
+LEFT JOIN   suppliers s ON p.supplier_id = s.id;
